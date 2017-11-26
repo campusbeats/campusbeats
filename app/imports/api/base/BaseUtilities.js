f014c35bcf0ed2e6dc5dfd7018316f37ed9d07c7
blaine-wataru <bwataru808@yahoo.com>
2017-11-25 15:39:11 -1000

    dropdown for abilities

--- app/imports/api/base/BaseUtilities.js
+++ app/imports/api/base/BaseUtilities.js
@@ -1,7 +1,9 @@
 import { Profiles } from '/imports/api/profile/ProfileCollection';
 import { Interests } from '/imports/api/interest/InterestCollection';
+import { Abilities } from '/imports/api/ability/AbilityCollection';
 
 export function removeAllEntities() {
   Profiles.removeAll();
   Interests.removeAll();
+  Abilities.removeAll();
 }
--- app/imports/ui/pages/user/profile-page.js
+++ app/imports/ui/pages/user/profile-page.js
@@ -3,7 +3,7 @@ import { ReactiveDict } from 'meteor/reactive-dict';
 import { FlowRouter } from 'meteor/kadira:flow-router';
 import { _ } from 'meteor/underscore';
 import { Profiles } from '/imports/api/profile/ProfileCollection';
-// import { Interests } from '/imports/api/interest/InterestCollection';
+import { Interests } from '/imports/api/interest/InterestCollection';
 import { Abilities } from '/imports/api/ability/AbilityCollection';
 
 const displaySuccessMessage = 'displaySuccessMessage';
