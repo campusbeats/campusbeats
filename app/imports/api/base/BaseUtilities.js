17b1e014b4ce787b7cae1fb26340bb6c680a178d
Lancen Daclison <lancenhd@hawaii.edu>
2017-11-25 15:30:48 -1000

    fail attempt of adding new stuff for goals collection. What am i missing?

--- app/imports/api/base/BaseUtilities.js
+++ app/imports/api/base/BaseUtilities.js
@@ -1,5 +1,7 @@
 import { Profiles } from '/imports/api/profile/ProfileCollection';
 import { Interests } from '/imports/api/interest/InterestCollection';
+import { Goals } from '/imports/api/goal/GoalCollection';
+
 
 export function removeAllEntities() {
   Profiles.removeAll();
