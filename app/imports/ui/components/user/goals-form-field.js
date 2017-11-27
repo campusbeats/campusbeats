64442e8316ad02d6a9cb6dc0b9edf274118d9019
Lancen Daclison <lancenhd@hawaii.edu>
2017-11-27 02:56:10 -1000

    meh

--- app/imports/ui/components/user/goals-form-field.js
+++
@@ -1,6 +0,0 @@
-import { Template } from 'meteor/templating';
-
-Template.Goals_Form_Field.onRendered(function onRendered() {
-  this.$('.dropdown').dropdown();
-});
-
--- app/imports/ui/pages/beats/beats-page.js
+++ app/imports/ui/pages/beats/beats-page.js
@@ -1,21 +1,97 @@
+/* import { Template } from 'meteor/templating';
+import { ReactiveDict } from 'meteor/reactive-dict';
+import { FlowRouter } from 'meteor/kadira:flow-router';
+import { _ } from 'meteor/underscore';
+import { Profiles } from '/imports/api/profile/ProfileCollection';
+import { Interests } from '/imports/api/interest/InterestCollection';
+
+const displaySuccessMessage = 'displaySuccessMessage';
+const displayErrorMessages = 'displayErrorMessages';
+
+Template.Beats_Page.onCreated(function onCreated() {
+  this.subscribe(Interests.getPublicationName());
+  this.subscribe(Profiles.getPublicationName());
+  this.messageFlags = new ReactiveDict();
+  this.messageFlags.set(displaySuccessMessage, false);
+  this.messageFlags.set(displayErrorMessages, false);
+  this.context = Profiles.getSchema().namedContext('Beats_Page');
+});
+
+Template.Beats_Page.helpers({
+  successClass() {
+    return Template.instance().messageFlags.get(displaySuccessMessage) ? 'success' : '';
+  },
+  displaySuccessMessage() {
+    return Template.instance().messageFlags.get(displaySuccessMessage);
+  },
+  errorClass() {
+    return Template.instance().messageFlags.get(displayErrorMessages) ? 'error' : '';
+  },
+  profile() {
+    return Profiles.findDoc(FlowRouter.getParam('username'));
+  },
+  interests() {
+    const profile = Profiles.findDoc(FlowRouter.getParam('username'));
+    const selectedInterests = profile.interests;
+    return profile && _.map(Interests.findAll(),
+        function makeInterestObject(interest) {
+          return { label: interest.name, selected: _.contains(selectedInterests, interest.name) };
+        });
+  },
+});
+
+
+Template.Beats_Page.events({
+  'submit .profile-data-form'(event, instance) {
+    event.preventDefault();
+    const firstName = event.target.First.value;
+    const lastName = event.target.Last.value;
+    const title = event.target.Title.value;
+    const username = FlowRouter.getParam('username'); // schema requires username.
+    const picture = event.target.Picture.value;
+    const github = event.target.Github.value;
+    const facebook = event.target.Facebook.value;
+    const instagram = event.target.Instagram.value;
+    const bio = event.target.Bio.value;
+    const selectedInterests = _.filter(event.target.Interests.selectedOptions, (option) => option.selected);
+    const interests = _.map(selectedInterests, (option) => option.value);
+
+    const updatedProfileData = { firstName, lastName, title, picture, github, facebook, instagram, bio, interests,
+      username };
+
+    // Clear out any old validation errors.
+    instance.context.reset();
+    // Invoke clean so that updatedProfileData reflects what will be inserted.
+    const cleanData = Profiles.getSchema().clean(updatedProfileData);
+    // Determine validity.
+    instance.context.validate(cleanData);
+
+    if (instance.context.isValid()) {
+      const docID = Profiles.findDoc(FlowRouter.getParam('username'))._id;
+      const id = Profiles.update(docID, { $set: cleanData });
+      instance.messageFlags.set(displaySuccessMessage, id);
+      instance.messageFlags.set(displayErrorMessages, false);
+    } else {
+      instance.messageFlags.set(displaySuccessMessage, false);
+      instance.messageFlags.set(displayErrorMessages, true);
+    }
+  },
+}); */
+
 import { Template } from 'meteor/templating';
 import { ReactiveDict } from 'meteor/reactive-dict';
 import { FlowRouter } from 'meteor/kadira:flow-router';
 import { _ } from 'meteor/underscore';
 import { Profiles } from '/imports/api/profile/ProfileCollection';
 import { Interests } from '/imports/api/interest/InterestCollection';
-import { Goals } from '/imports/api/goal/GoalCollection';
 
 const selectedInterestsKey = 'selectedInterests';
-const selectedGoalsKey = 'selectedGoals';
 
 Template.Beats_Page.onCreated(function onCreated() {
   this.subscribe(Interests.getPublicationName());
   this.subscribe(Profiles.getPublicationName());
-  this.subscribe(Goals.getPublicationName());
   this.messageFlags = new ReactiveDict();
   this.messageFlags.set(selectedInterestsKey, undefined);
-  this.messageFlags.set(selectedGoalsKey, undefined);
   this.context = Profiles.getSchema().namedContext('Beats_Page');
 });
 
@@ -46,24 +122,8 @@ Template.Beats_Page.helpers({
           return { label: interest.name, selected: _.contains(selectedInterests, interest.name) };
         });
   },
-  goals() {
-    /*  return _.map(Goals.findAll(),
-        function makeGoalObject(goal) {
-          return {
-            label: goal.name,
-            selected: _.contains(Template.instance().messageFlags.get(selectedGoalsKey), goal.name),
-          };
-        }); */
-    const profile = Profiles.findDoc(FlowRouter.getParam('username'));
-    const selectedGoals = profile.goals;
-    return profile && _.map(Goals.findAll(),
-        function makeGoalObject(goal) {
-          return { label: goal.name, selected: _.contains(selectedGoals, goal.name) };
-        });
-  },
 });
 
-/* I feel like I might need to change this if I want to have it register the filtering. */
 Template.Beats_Page.events({
   'submit .filter-data-form'(event, instance) {
     event.preventDefault();
