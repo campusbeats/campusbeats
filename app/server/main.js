e368b1ed5d5d330568b0684e03053412ce799cee
Lancen Daclison <lancenhd@hawaii.edu>
2017-11-27 04:34:12 -1000

    forgot to delete head in one part.

--- app/server/main.js
+++ app/server/main.js
@@ -2,13 +2,9 @@ import '/imports/startup/server';
 import '/imports/api/base';
 import '/imports/api/profile';
 import '/imports/api/interest';
-<<<<<<< HEAD
 import '/imports/api/ability';
 import '/imports/api/style';
 import '/imports/api/favorites';
 import '/imports/api/people-interested';
-
-=======
 import '/imports/api/goal';
 import '/imports/api/experience';
->>>>>>> issue-20e
