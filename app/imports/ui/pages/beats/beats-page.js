d8e8d351da73a9b7f3141d6f35ca2ccda3b4b326
blaine-wataru <bwataru808@yahoo.com>
2017-11-27 21:17:45 -1000

    updates collections in cards and beats page, updated profile collection

--- app/imports/ui/pages/beats/beats-page.js
+++ app/imports/ui/pages/beats/beats-page.js
@@ -5,21 +5,29 @@ import { _ } from 'meteor/underscore';
 import { Profiles } from '/imports/api/profile/ProfileCollection';
 import { Interests } from '/imports/api/interest/InterestCollection';
 import { Goals } from '/imports/api/goal/GoalCollection';
+import { Abilities } from '/imports/api/ability/AbilityCollection';
+import { Styles } from '/imports/api/style/StyleCollection';
 import { Experiences } from '/imports/api/experience/ExperienceCollection';
 
 const selectedInterestsKey = 'selectedInterests';
 const selectedGoalsKey = 'selectedGoals';
 const selectedExperiencesKey = 'selectedExperiences';
+const selectedAbilitiesKey = 'selectedAbilities';
+const selectedStylesKey = 'selectedStyles';
 
 Template.Beats_Page.onCreated(function onCreated() {
   this.subscribe(Interests.getPublicationName());
   this.subscribe(Profiles.getPublicationName());
   this.subscribe(Goals.getPublicationName());
   this.subscribe(Experiences.getPublicationName());
+  this.subscribe(Abilities.getPublicationName());
+  this.subscribe(Styles.getPublicationName());
   this.messageFlags = new ReactiveDict();
   this.messageFlags.set(selectedInterestsKey, undefined);
   this.messageFlags.set(selectedGoalsKey, undefined);
   this.messageFlags.set(selectedExperiencesKey, undefined);
+  this.messageFlags.set(selectedAbilitiesKey, undefined);
+  this.messageFlags.set(selectedStylesKey, undefined);
   this.context = Profiles.getSchema().namedContext('Beats_Page');
 });
 
@@ -36,7 +44,7 @@ Template.Beats_Page.helpers({
   },
 
   interests() {
-    /*  return _.map(Interests.findAll(),
+      /*  return _.map(Interests.findAll(),
         function makeInterestObject(interest) {
           return {
             label: interest.name,
@@ -78,6 +86,36 @@ Template.Beats_Page.helpers({
     return profile && _.map(Experiences.findAll(),
         function makeExperienceObject(experience) {
           return { label: experience.name, selected: _.contains(selectedExperiences, experience.name) };
+        });
+  },
+  abilities() {
+    /*  return _.map(Goals.findAll(),
+        function makeGoalObject(goal) {
+          return {
+            label: goal.name,
+            selected: _.contains(Template.instance().messageFlags.get(selectedGoalsKey), goal.name),
+          };
+        }); */
+    const profile = Profiles.findDoc(FlowRouter.getParam('username'));
+    const selectedAbilities = profile.abilities;
+    return profile && _.map(Abilities.findAll(),
+        function makeAbilityObject(ability) {
+          return { label: ability.name, selected: _.contains(selectedAbilities, ability.name) };
+        });
+  },
+  styles() {
+    /*  return _.map(Goals.findAll(),
+        function makeGoalObject(goal) {
+          return {
+            label: goal.name,
+            selected: _.contains(Template.instance().messageFlags.get(selectedGoalsKey), goal.name),
+          };
+        }); */
+    const profile = Profiles.findDoc(FlowRouter.getParam('username'));
+    const selectedStyles = profile.styles;
+    return profile && _.map(Styles.findAll(),
+        function makeStyleObject(style) {
+          return { label: style.name, selected: _.contains(selectedStyles, style.name) };
         });
   },
 });
