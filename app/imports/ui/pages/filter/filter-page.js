1cc7be8134484365329eca82302a313db18ec0ed
Lancen Daclison <lancenhd@hawaii.edu>
2017-12-03 22:53:32 -1000

    tried to focus on just goals, but messed it up

--- app/imports/ui/pages/filter/filter-page.js
+++ app/imports/ui/pages/filter/filter-page.js
@@ -3,44 +3,74 @@ import { ReactiveDict } from 'meteor/reactive-dict';
 import { _ } from 'meteor/underscore';
 import { Profiles } from '/imports/api/profile/ProfileCollection';
 import { Interests } from '/imports/api/interest/InterestCollection';
+import { Goals } from '/imports/api/goal/GoalCollection';
 
 const selectedInterestsKey = 'selectedInterests';
+const selectedGoalsKey = 'selectedGoals';
 
 Template.Filter_Page.onCreated(function onCreated() {
+  this.subscribe(Goals.getPublicationName());
   this.subscribe(Interests.getPublicationName());
   this.subscribe(Profiles.getPublicationName());
   this.messageFlags = new ReactiveDict();
   this.messageFlags.set(selectedInterestsKey, undefined);
+  this.messageFlags.set(selectedGoalsKey, undefined);
 });
 
 Template.Filter_Page.helpers({
   profiles() {
     // Initialize selectedInterests to all of them if messageFlags is undefined.
-    if (!Template.instance().messageFlags.get(selectedInterestsKey)) {
+    if (!Template.instance().messageFlags.get(selectedInterestsKey) &&
+        !Template.instance().messageFlags.get(selectedGoalsKey)) {
       Template.instance().messageFlags.set(selectedInterestsKey, _.map(Interests.findAll(), interest => interest.name));
+      Template.instance().messageFlags.set(selectedGoalsKey, _.map(Goals.findAll(), goal => goal.name));
     }
     // Find all profiles with the currently selected interests.
-    const allProfiles = Profiles.findAll();
+    let allProfiles = Profiles.findAll();
     const selectedInterests = Template.instance().messageFlags.get(selectedInterestsKey);
-    return _.filter(allProfiles, profile => _.intersection(profile.interests, selectedInterests).length > 0);
+    const selectedGoals = Template.instance().messageFlags.get(selectedGoalsKey);
+    allProfiles =_.filter(allProfiles, profile => _.intersection(profile.interests, selectedInterests).length > 0);
+    allProfiles =_.filter(allProfiles, profile => _.intersection(profile.goals, selectedGoals).length > 0);
   },
 
   interests() {
-    return _.map(Interests.findAll(),
+    /* return _.map(Interests.findAll(),
         function makeInterestObject(interest) {
           return {
             label: interest.name,
             selected: _.contains(Template.instance().messageFlags.get(selectedInterestsKey), interest.name),
           };
+        }); */
+    const profile = Profiles.findDoc(FlowRouter.getParam('username'));
+    const selectedInterests = profile.interests;
+    return profile && _.map(Interests.findAll(),
+        function makeInterestObject(interest) {
+          return { label: interest.name, selected: _.contains(selectedInterests, interest.name) };
+        });
+  },
+  goals() {
+    /*  return _.map(Goals.findAll(),
+        function makeGoalObject(goal) {
+          return {
+            label: goal.name,
+            selected: _.contains(Template.instance().messageFlags.get(selectedGoalsKey), goal.name),
+          };
+        }); */
+    const profile = Profiles.findDoc(FlowRouter.getParam('username'));
+    const selectedGoals = profile.goals;
+    return profile && _.map(Goals.findAll(),
+        function makeGoalObject(goal) {
+          return { label: goal.name, selected: _.contains(selectedGoals, goal.name) };
         });
   },
 });
-
 Template.Filter_Page.events({
   'submit .filter-data-form'(event, instance) {
     event.preventDefault();
     const selectedOptions = _.filter(event.target.Interests.selectedOptions, (option) => option.selected);
     instance.messageFlags.set(selectedInterestsKey, _.map(selectedOptions, (option) => option.value));
+    const selectedOptions2 = _.filter(event.target.Goals.selectedOptions2, (option) => option.selected);
+    instance.messageFlags.set(selectedGoalsKey, _.map(selectedOptions2, (option) => option.value));
   },
 });
 
