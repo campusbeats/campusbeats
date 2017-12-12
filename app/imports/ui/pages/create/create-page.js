8b40664e8a90d92195e3c8d2e3ec946023482639
innikakp <innikakp@hawaii.edu>
2017-12-11 20:40:43 -1000

    Removed unncessary information
    Trying to format it like digits and insert event for calendar function

--- app/imports/ui/pages/create/create-page.js
+++ app/imports/ui/pages/create/create-page.js
@@ -2,25 +2,26 @@ import { Template } from 'meteor/templating';
 import { ReactiveDict } from 'meteor/reactive-dict';
 import { FlowRouter } from 'meteor/kadira:flow-router';
 import { _ } from 'meteor/underscore';
-import { Profiles } from '/imports/api/profile/ProfileCollection';
+// import { Profiles } from '/imports/api/profile/ProfileCollection';
 import { Abilities } from '/imports/api/ability/AbilityCollection';
-import { Styles } from '/imports/api/style/StyleCollection';
+/* import { Styles } from '/imports/api/style/StyleCollection';
 import { Goals } from '/imports/api/goal/GoalCollection';
-import { Experiences } from '/imports/api/experience/ExperienceCollection';
+import { Experiences } from '/imports/api/experience/ExperienceCollection'; */
 
 const displaySuccessMessage = 'displaySuccessMessage';
 const displayErrorMessages = 'displayErrorMessages';
 const selectedAbilitiesKey = 'selectedAbilities';
 const selectedStylesKey = 'selectedStyles';
 const selectedGoalsKey = 'selectedGoals';
-const selectedExperiencesKey = 'selectedExperiences';
+const createContext = Abilities.getSchema().namedContext('Create_Page');
 
+const selectedExperiencesKey = 'selectedExperiences';
 Template.Create_Page.onCreated(function onCreated() {
-  this.subscribe(Abilities.getPublicationName());
+  /* this.subscribe(Abilities.getPublicationName());
   this.subscribe(Styles.getPublicationName());
   this.subscribe(Profiles.getPublicationName());
   this.subscribe(Goals.getPublicationName());
-  this.subscribe(Experiences.getPublicationName());
+  this.subscribe(Experiences.getPublicationName()); */
   this.messageFlags = new ReactiveDict();
   this.messageFlags.set(displaySuccessMessage, false);
   this.messageFlags.set(displayErrorMessages, false);
@@ -28,7 +29,7 @@ Template.Create_Page.onCreated(function onCreated() {
   this.messageFlags.set(selectedStylesKey, undefined);
   this.messageFlags.set(selectedGoalsKey, undefined);
   this.messageFlags.set(selectedExperiencesKey, undefined);
-  this.context = Abilities.getSchema().namedContext('Create_Page');
+  this.context = createContext;
 });
 
 Template.Create_Page.helpers({
@@ -41,53 +42,12 @@ Template.Create_Page.helpers({
   errorClass() {
     return Template.instance().messageFlags.get(displayErrorMessages) ? 'error' : '';
   },
-  profile() {
-    return Profiles.findDoc(FlowRouter.getParam('username'));
-  },
-  abilities() {
-    /* return _.map(Abilities.findAll(),
-        function makeAbilityObject(ability) {
-          return {
-            label: ability.name,
-            selected: _.contains(Template.instance().messageFlags.get(selectedAbilitiesKey), ability.name),
-          };
-        }); */
-    const profile = Profiles.findDoc(FlowRouter.getParam('username'));
-    const selectedAbilities = profile.abilities;
-    return profile && _.map(Abilities.findAll(),
-        function makeAbilityObject(ability) {
-          return { label: ability.name, selected: _.contains(selectedAbilities, ability.name) };
-        });
-  },
-  styles() {
-    const profile = Profiles.findDoc(FlowRouter.getParam('username'));
-    const selectedStyles = profile.styles;
-    return profile && _.map(Styles.findAll(),
-        function makeStyleObject(style) {
-          return { label: style.name, selected: _.contains(selectedStyles, style.name) };
-        });
-  },
-  goals() {
-    const profile = Profiles.findDoc(FlowRouter.getParam('username'));
-    const selectedGoals = profile.goals;
-    return profile && _.map(Goals.findAll(),
-        function makeGoalObject(goal) {
-          return { label: goal.name, selected: _.contains(selectedGoals, goal.name) };
-        });
-  },
-  experiences() {
-    const profile = Profiles.findDoc(FlowRouter.getParam('username'));
-    const selectedExperiences = profile.experiences;
-    return profile && _.map(Experiences.findAll(),
-        function makeExperienceObject(experience) {
-          return { label: experience.name, selected: _.contains(selectedExperiences, experience.name) };
-        });
-  },
 });
 
 Template.Create_Page.events({
   'submit .create-data-form'(event, instance) {
     event.preventDefault();
+    // let newAbility = Session.get('abilities');
     const name = event.target.Name.value;
     const description = event.target.Description.value;
     /* const selectedAbilities = _.filter(event.target.Abilities.selectedOptions, (option) => option.selected);
@@ -100,25 +60,27 @@ Template.Create_Page.events({
     const selectedExperiences = _.filter(event.target.Experiences.selectedOptions, (option) => option.selected);
     const experiences = _.map(selectedExperiences, (option) => option.value); */
     // const updatedAbilitiesData = { abilities, username };
-    const updatedAbilitiesData = { name, description };
+    // const newAbility = { name, description };
+    const newAbility = { name, description };
 
     // Clear out any old validation errors.
     instance.context.reset();
     // Invoke clean so that updatedProfileData reflects what will be inserted.
-    const cleanData = Abilities.getSchema().clean(updatedAbilitiesData);
+    const cleanData = Abilities.getSchema().clean(newAbility);
     // Determine validity.
     instance.context.validate(cleanData);
 
     if (instance.context.isValid()) {
       // const docID = Abilities._id;
       // const id = Abilities.insert(docID, { $set: cleanData });
-      const id = Abilities.insert(cleanData);
+      Abilities.insert(newAbility);
       // Abilities.insert(docID, cleanData);
       // const id = Abilities.insert(updatedAbilitiesData);
       // const id = Abilities.insert({ createdBy: Meteor.userId(), cleanData});
       // Abilities.insert(cleanData);
-      instance.messageFlags.set(displaySuccessMessage, id);
+      // instance.messageFlags.set(displaySuccessMessage, id);
       instance.messageFlags.set(displayErrorMessages, false);
+      FlowRouter.go('Home_Page');
     } else {
       instance.messageFlags.set(displaySuccessMessage, false);
       instance.messageFlags.set(displayErrorMessages, true);
