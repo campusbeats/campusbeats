defbee8c7573878996cd6cbcffee1fee39dfd16d
blaine-wataru <bwataru808@yahoo.com>
2017-12-13 15:42:38 -1000

    eslint updates

--- app/imports/api/eventdata/methods.js
+++ app/imports/api/eventdata/methods.js
@@ -1,17 +1,17 @@
-import SimpleSchema from 'simpl-schema';
+// import SimpleSchema from 'simpl-schema';
 import { Meteor } from 'meteor/meteor';
-import { EventData, EventDataSchema } from '/imports/api/eventdata/eventdata';
+import { EventData } from '/imports/api/eventdata/eventdata';
 
 
 Meteor.methods({
   // Allows an event to be edited.
-  editEvent( event ) {
+  editEvent(event) {
     try {
       return EventData.update( event._id, {
-        $set: event
+        $set: event,
       });
-    } catch ( exception ) {
-      throw new Meteor.Error( '500', `${ exception }` );
+    } catch (exception) {
+      throw new Meteor.Error('500', `${exception}`);
     }
   },
 });
